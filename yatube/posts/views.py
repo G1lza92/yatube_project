077b22cf49da606dff3f8aaf110d15a4f46bdd0f
Артем Зимин <wade_92@mail.ru>
2022-06-01 20:38:30 +0300

    Create urls and vievs-functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+# Посты отфильтрованные по группам
+def group_posts(request, slug):
+    return HttpResponse('Посты отфильтрованные по группам')
